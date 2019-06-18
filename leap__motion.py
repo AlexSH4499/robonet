387c10c135d0e993d34eafde2184825400607206
Alejandro <rodrigueznatalandy@gmail.com>
2019-06-18 13:16:13 -0700

    Added a wrapper of leap motion but man is it a headache, doesnt work with window

--- leap__motion.py
+++ leap__motion.py
@@ -1,13 +1,50 @@
-import os, sys, inspect, thread, time
+import os, sys, inspect, _thread, time
+
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-# Windows and Linux
-arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
-# Mac
-#arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+# src_dir = os.path.abspath("C:\\Leap_Motion_Developer_Kit_4.0.0+52173\\LeapSDK")
+# # Windows and Linux
+#arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+# # Mac
+# #arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+#sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, '../leap_python3')))
+#sys.path.append(r"C:\Leap_Motion_Developer_Kit_4.0.0+52173\LeapSDK\lib\x64")
+# sys.path.append(r"C:\Leap_Motion_Developer_Kit_4.0.0+52173\LeapSDK")
+# sys.path.append(r"C:\Leap_Motion_Developer_Kit_4.0.0+52173\LeapSDK\lib")
+# sys.path.append(r"C:\Leap_Motion_Developer_Kit_4.0.0+52173\LeapSDK\lib\x86")
+from leap_python3 import Leap
 
-sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
-import Leap
+class ContextAwareListener(Leap.Listener):
+
+    def __init__(self, controller=None):
+        if controller is None:
+            raise ValueError("Controller was not properly intialized...")
+
+        super().__init__()
+        self.controller = controller
+
+    def __enter__(self):
+        self.controller.add_listener(self)
+
+    def __exit__(self):
+        self.controller.remove_listener(self)
+
+    def on_connect(self,controller):
+        print("Connected..\n\n")
+
+    def on_frame(self,controller):
+        print("Frame available\n")
+        frame = controller.frame()
+        previous = controller.frame(1)
+
+        #do something with the hand position inside the frame
+        hand_props = hand_properties(frame)
+
+        # with open('movement.txt', 'a+') as file:
+        #     file.write(hand_props)
+        return frame
+
 
 def hand_properties(frame):
     hand = frame.hands.rightmost
@@ -22,11 +59,11 @@ def hand_properties(frame):
     return position, pitch, yaw, roll
 
 
-def follow_hand(handID=0, frame):
+def follow_hand(handID=0, frame=None):
     hand = frame.hand(handID)
     pass
 
-def is_previous(curr_frame,prev_id):
+def is_previous_frame(curr_frame,prev_id):
     if curr_frame.id == prev_id:
         return True
     return False
@@ -35,51 +72,31 @@ def main():
 
     # listener = ContextAwareListener()
     controller = Leap.Controller()
-
-    while not controller.is_connected():
-        #wait here doing whatever
-        print("...")
+    #
+    # while not controller.is_connected():
+    #     #wait here doing whatever
+    #     print("...")
     #print("Controller Connected...\n\n")
     with ContextAwareListener(controller) as listener:
     # controller.add_listener(listener)
 
         # Keep this process running until Enter is pressed
         print("Press Enter to quit...")
-        try:
-            sys.stdin.readline()
-        except KeyboardInterrupt:
-            pass
+        current_line = sys.stdin.readline().strip()
+        print(current_line)
+        #while current_line  is None:
+        while True:
+            
+            frame = listener.on_frame(listener.controller)
+            print(hand_properties(frame))
+            try:
+
+                current_line = sys.stdin.readline()
+            except KeyboardInterrupt:
+                print("Terminating Leap Motion Connection...")
 
     # finally:
     #     controller.remove_listener(listener)
 
 if __name__ == "__main__":
     main()
-
-class ContextAwareListener(Leap.Listener):
-
-    def __init__(self, controller=None):
-        self.super().controller = controller
-
-    def __enter__(self):
-        self.controller.add_listener(listener)
-
-
-    def __exit__(self):
-        self.controller.remove_listener(self)
-
-
-    def on_connect(self,controller):
-        print("Connected..\n\n")
-
-    def on_frame(self,controller):
-        print("Frame available\n")
-        frame = controller.frame()
-        previous = controller.frame(1)
-
-        #do something with the hand position inside the frame
-        hand_props = hand_properties(frame)
-
-        with open('movement.txt', 'a+') as file:
-            file.write(hand_props)
-        return
