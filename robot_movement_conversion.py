d5e14a654d012850bcc8ce1076ae020128c68be6
Alejandro <rodrigueznatalandy@gmail.com>
2019-06-24 07:38:40 -0700

    Added a dummy api to test robot

--- robot_movement_conversion.py
+++ robot_movement_conversion.py
@@ -2,11 +2,10 @@ import math
 
 
 import numpy as np
-
-
+from niryo_one_python_api.niryo_one_api import *
+import rospy
 
 def ensure_length(coords=[]):
-
     return len(coords) == 6
 
 def cartesian_to_radians(coords=[]):
