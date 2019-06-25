411023363be60b81efa0e8176a2ec0b489f8f837
Alejandro <rodrigueznatalandy@gmail.com>
2019-06-25 07:20:26 -0700

    Added virtual environments for 2.7 and 3.7 and dependencies for 3.7, still missing 2.7 dependencies

--- RESTAPI/roboIF/admin.py
+++ RESTAPI/roboIF/admin.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+
 from django.contrib import admin
 
-from .models import Robot, WareHouse
+from .models import Robot, WareHouse, MovementRequest
 
 
 # Register your models here.
 admin.site.register(Robot)
 admin.site.register(WareHouse)
+admin.site.register(MovementRequest)
--- RESTAPI/roboIF/urls.py
+++ RESTAPI/roboIF/urls.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from django.urls import path, include
 # from views import RobotList, RobotView
@@ -8,6 +9,7 @@ from rest_framework.urlpatterns import  format_suffix_patterns
 router = routers.DefaultRouter()
 router.register(r'robots', views.RobotView)
 router.register(r'warehouses', views.WareHouseView)
+router.register(r'requests',views.RequestsView)
 
 urlpatterns = [
     #path(r'/robots', RobotList.as_view()),
