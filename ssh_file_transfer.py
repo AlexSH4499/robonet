4ebf0b162e1ca9742cc42ddfb6f425a8656c326f
Alejandro <rodrigueznatalandy@gmail.com>
2019-07-09 11:35:45 -0700

    Cleaned up ssh-file-transfer file and forgot to say, problems found while importing paramiko, made a pip freeze with python2.7 such that we don't deal with these in the future

--- ssh_file_transfer.py
+++ ssh_file_transfer.py
@@ -1,24 +1,8 @@
-import paramiko, base64
+
 
 ROBOT_IP_ADDRESS = '192.168.1.36'
-USER= ''
-PASS = ''
-def  start_client():
-
-    key = paramiko.RSAKey(data=base64.b64decode(b'AAA...'))
-    client = paramiko.SSHClient()
-    client.get_host_keys().add(ROBOT_IP_ADDRESS, 'ssh-rsa' , key)
-    client.connect(ROBOT_IP_ADDRESS,username=USER, password=PASS)
-
-    #Commands here
-    stdin, stdout, stderr = client.exec_command('ls')
-
-    for line in stdout:
-        print('...' + line.strip('\n'))
-
-    client.close()
-
-
+USER= 'niryo'
+PASS = 'robotics'
 
 # Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
 #
@@ -105,7 +89,7 @@ def host_key_and_type(hostkeytype=None, hostkey=None):
         except IOError:
             print("*** Unable to open host keys file")
             host_keys = {}
-
+    hostname=ROBOT_IP_ADDRESS
     if hostname in host_keys:
         hostkeytype = host_keys[hostname].keys()[0]
         hostkey = host_keys[hostname][hostkeytype]
@@ -116,36 +100,14 @@ def host_key_and_type(hostkeytype=None, hostkey=None):
 
 def send_file(hostname=ROBOT_IP_ADDRESS, hostkey=None, username="niryo", password="robotics", file_to_send="demo_sftp.py", directory="demo_sftp_folder"):
     t = paramiko.Transport((hostname, Port))
-    # t.connect(hostkey,  username,   password,gss_host=socket.getfqdn(hostname),
-    #             gss_auth=UseGSSAPI, gss_kex=DoGSSAPIKeyExchange,)
     t.connect(hostkey, username,   password,)
     sftp = paramiko.SFTPClient.from_transport(t)
 
     # dirlist on remote host
-    dirlist = sftp.listdir(directory)
-    print("Dirlist: %s" % dirlist)
-
-    # copy this demo onto the server
-    # try:
-    #     sftp.mkdir(directory)
-    # except IOError:
-    #     print("(assuming demo_sftp_folder/ already exists)")
-    # with sftp.open("demo_sftp_folder/README", "w") as f:
-    #     f.write("This was created by demo_sftp.py.\n")
-    # with open(file_to_send, "r") as f:
-    #     data = f.read()
-    # sftp.open("demo_sftp_folder/demo_sftp.py", "w").write(data)
-    # print("created demo_sftp_folder/ on the server")
-
-    # # copy the README back here
-    # with sftp.open("demo_sftp_folder/README", "r") as f:
-    #     data = f.read()
-    # with open("README_demo_sftp", "w") as f:
-    #     f.write(data)
-    # print("copied README back here")
-
+    # dirlist = sftp.listdir(directory)
+    # print("Dirlist: %s" % dirlist)
     # BETTER: use the get() and put() methods
-    print(sftp.put(localpath=file_to_send,remotepath=directory+file_to_send, confirm=True ))
+    print(sftp.put(localpath=file_to_send,remotepath=directory+file_to_send, confirm=True ))#remote directory must include file to be sent in the path
     # sftp.get("demo_sftp_folder/README", "README_demo_sftp")
 
     t.close()
@@ -156,32 +118,10 @@ def send_file(hostname=ROBOT_IP_ADDRESS, hostkey=None, username="niryo", passwor
 def connect():
     # now, connect and use paramiko Transport to negotiate SSH2 across the connection
     try:
-        # username, password = auth_credentials()
-        # hostkeytype, hostkey = host_key_and_type()
-
-        # t = paramiko.Transport((ROBOT_IP_ADDRESS, Port))
-        # t.connect(
-        #     None,
-        #     "niryo",
-        #     "robotics",
-        #     # gss_host=socket.getfqdn(ROBOT_IP_ADDRESS),
-        #     # gss_auth=UseGSSAPI,
-        #     # gss_kex=DoGSSAPIKeyExchange,
-        # )
-        # sftp = paramiko.SFTPClient.from_transport(t)
-
-        # # dirlist on remote host
-        # dirlist = sftp.listdir(".")
-        # print("Dirlist: %s" % dirlist)
-        # copy this demo onto the server
         send_file(hostname=ROBOT_IP_ADDRESS, hostkey=None, username="niryo",password="robotics", file_to_send="niryo_one_example_python_api.py",directory="/home/niryo/catkin_ws/devel")
     except Exception as e:
         print("*** Caught exception: %s: %s" % (e.__class__, e))
         traceback.print_exc()
-        # try:
-        #    # t.close()
-        # except:
-        #     pass
         sys.exit(1)
 
     return
