0572a18735ef769ee17dcb63ef585c79fb9413f4
Alejandro <rodrigueznatalandy@gmail.com>
2019-07-29 11:08:36 -0700

    Cleanup of unused scripts and folders

--- robot_remote.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function query_files{
-  echo "Querying updated robot scripts...\n"
-  export developmentenv="/catkin_ws/devel"
-  cd $developmentenv
-}
-
-ssh niryo@192.168.1.42 'source ~/catkin_ws/devel/setup.bash && export PYTHONPATH=${PYTHONPATH}:/home/niryo/catkin_ws/src/niryo_one_python_api/src/niryo_python_api && python niryo_one_example_python_api.py'
